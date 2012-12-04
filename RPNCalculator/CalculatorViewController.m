79289f70a6891163c9bd7aa0461a31db7fb338cb
MACKITRON <mackitikorn@gmail.com>
2012-12-04 19:40:09 +0700

    digitPressed: will get executed whenever any of these buttons gets touched.

--- RPNCalculator/CalculatorViewController.m
+++ RPNCalculator/CalculatorViewController.m
@@ -9,52 +9,13 @@
 #import "CalculatorViewController.h"
 
 @implementation CalculatorViewController
+@synthesize display = _display;
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
+- (IBAction)digitPressed:(UIButton *)sender {
+    
+    NSString *digit = [sender currentTitle];
+    
+    self.display.text = digit;
+    NSLog(@"user touched %@", digit);
 }
-
-#pragma mark - View lifecycle
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-}
-
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
-}
-
-- (void)viewWillDisappear:(BOOL)animated
-{
-	[super viewWillDisappear:animated];
-}
-
-- (void)viewDidDisappear:(BOOL)animated
-{
-	[super viewDidDisappear:animated];
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    // Return YES for supported orientations
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-}
-
 @end
