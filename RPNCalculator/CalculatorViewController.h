9af16fdc798e857e93754da693d5ae622417a96c
MACKITRON <mackitikorn@gmail.com>
2012-12-04 19:25:12 +0700

    Initial Commit

---
+++ RPNCalculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  RPNCalculator
+//
+//  Created by Mac KC on 12/4/12.
+//  Copyright (c) 2012 Apple Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
---
+++ RPNCalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RPNCalculator
+//
+//  Created by Mac KC on 12/4/12.
+//  Copyright (c) 2012 Apple Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CalculatorAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CalculatorAppDelegate class]));
+    }
+}
